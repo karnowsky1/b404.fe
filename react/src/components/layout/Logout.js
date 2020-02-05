bc19674f699c51c198e059aa2a72fd006eefe47e
Bubba Lichvar <bubbathevtog@gmail.com>
2020-02-05 18:17:00 -0500

    removed other logout menu settings after conversation w/ mislav

--- react/src/components/layout/Logout.js
+++ react/src/components/layout/Logout.js
@@ -16,9 +16,7 @@ class Logout extends React.Component {
   render() {
     const { setIsLoggedIn } = this.props
     const profile_icon = 'user'
-    // const SETTINGS = "2"
-    const LOG_OUT = "3"  
-    // const logout_icon = 'logout'
+    const LOG_OUT = "2"  
 
     function handleMenuClick(e) {
       console.log('click', e);
@@ -33,11 +31,6 @@ class Logout extends React.Component {
     const menu = (
       <Menu onClick={handleMenuClick}>
         <Menu.Item key="2">
-          <NavLink to="/main-settings">
-            Settings
-          </NavLink>
-        </Menu.Item>
-        <Menu.Item key="3">
           <NavLink to="/login">
             Log Out
           </NavLink>
@@ -46,13 +39,6 @@ class Logout extends React.Component {
     )
     
     return(
-      /*
-      <Dropdown overlay={menu}>
-        <Button style={{borderColor: '#001529'}} type="primary" shape="circle">
-          <Icon style= {{ color: '#001529' }} type={profile_icon} />
-        </Button>
-      </Dropdown>
-      */
       <Dropdown overlay={menu}>
         <Button style={{borderColor: '#542711', boxShadow: '0 0 1px 0px #542711 inset, 0 0 1px 0px #542711' }} type="primary" shape="circle">
           <Icon style= {{ color: '#542711' }} type={profile_icon} />
