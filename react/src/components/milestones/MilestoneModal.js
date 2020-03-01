d420f5030cf6f4f47d75738fed6240c8cf24ba56
karnowsky1 <kestime9015@bths.edu>
2020-03-01 15:48:37 -0500

    minor css changes to logout button/ header and spacing changes in index.css,  admin People deletion message change, more validation within PeopleModal and MilestoneModal, user cab now add milestones to the syestem

--- react/src/components/milestones/MilestoneModal.js
+++ react/src/components/milestones/MilestoneModal.js
@@ -1,7 +1,7 @@
 import { Formik } from 'formik';
 import { Input, Form, SubmitButton, Select } from 'formik-antd';
-import { Form as AntForm, Modal } from 'antd'
-import React from 'react';
+import { Form as AntForm, Modal } from 'antd';
+import React, { useState } from 'react';
 import { required } from '../../utils/validators';
 import { DateRange } from '../../utils/DateRange';
 
@@ -10,11 +10,8 @@ const { Option } = Select;
 const defaults = {
   name: '',
   description: '',
-  startDate: '',
-  deliveryDate: '',
-  company: undefined,
-  accessLevelID: ''
-}
+  companyID: undefined
+};
 
 const MilestoneModalForm = ({
   initialValues = defaults,
@@ -24,61 +21,91 @@ const MilestoneModalForm = ({
   title,
   isAddModal,
   form
-}) => (
-  <Modal title={title} visible footer={[]} onCancel={onCancel}>
-    <Formik
-      initialValues={initialValues}
-      onSubmit={(values, { setSubmitting }) => { // onSubmit from formik-antd
-        onSubmit(values) // onSubmit passed from props 
+}) => {
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [failedSubmit, setFailedSubmit] = useState({
+    startDate: false,
+    endDate: false
+  });
+
+  return (
+    <Modal title={title} visible footer={[]} onCancel={onCancel}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, { setSubmitting }) => {
+          // onSubmit from formik-antd
+          if (!!startDate && !!endDate) {
+            onSubmit({
+              ...values,
+              startDate: startDate.format('YYYY-MM-DD HH:mm:ss'),
+              deliveryDate: endDate.format('YYYY-MM-DD HH:mm:ss')
+            }); // onSubmit passed from props
+          }
           setSubmitting(false);
-      }}
-    >
-      {({ handleSubmit }) => (
-        <Form
-          onSubmit={e => {
-            e.preventDefault();
-            handleSubmit();
-          }}
-        >
-          <p>Name *</p>
-          <Form.Item name="name" validate={required}>
-            <Input name="name" placeholder="Milestone 1" />
-          </Form.Item>
-          <p></p>
-          <p>Description *</p>
-          <Form.Item name="description">
-            <Input name="description" placeholder="Milestone Description" />
-          </Form.Item>
-          <p></p>
-          <p>Company</p>
-          {/* //////////////////////////////////////////////////// */}
-          <Select
-            // mode="multiple"
-            name="company"
-            style={{ width: '100%' }}
-            placeholder="Select Company"
-            defaultValue={initialValues.company}
-            validate={required}
+        }}
+      >
+        {({ handleSubmit }) => (
+          <Form
+            onSubmit={e => {
+              e.preventDefault();
+              if (!startDate || !endDate) {
+                setFailedSubmit({
+                  startDate: !startDate,
+                  endDate: !endDate
+                });
+              }
+              handleSubmit();
+            }}
           >
-            {/* { console.log(companies)} */}
-            {/* {console.log(initialValues.company)} */}
+            <p>Name *</p>
+            <Form.Item name="name" validate={required}>
+              <Input name="name" placeholder="Milestone 1" />
+            </Form.Item>
+            <p></p>
+            <p>Description</p>
+            <Form.Item name="description">
+              <Input name="description" placeholder="Milestone Description" />
+            </Form.Item>
+            <p></p>
+            <p>Company *</p>
+            {/* //////////////////////////////////////////////////// */}
+            <Form.Item name="companyID" validate={required}>
+              <Select
+                name="companyID"
+                style={{ width: '100%' }}
+                placeholder="Select Company"
+                defaultValue={initialValues.company}
+              >
+                {/* { console.log(companies)} */}
+                {/* {console.log(initialValues.company)} */}
 
+                {companies.map(({ value, label }) => (
+                  <Option value={value} key={value}>
+                    {label}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+            {/* //////////////////////////////////////////////////// */}
+            <p></p>
+            <DateRange
+              startDate={startDate}
+              endDate={endDate}
+              setStartDate={setStartDate}
+              setEndDate={setEndDate}
+              failedSubmit={failedSubmit}
+              setFailedSubmit={setFailedSubmit}
+            />
+            <p></p>
+            <SubmitButton>Submit</SubmitButton>
+          </Form>
+        )}
+      </Formik>
+    </Modal>
+  );
+};
 
-            {companies.map(({ value, label }) => (
-               <Option value={value} key={value}> 
-                {label} 
-               </Option> 
-            ))}
-          </Select>
-          {/* //////////////////////////////////////////////////// */}
-          <p></p>
-          <DateRange></DateRange>
-          <p></p>
-          <SubmitButton>Submit</SubmitButton>
-        </Form>
-      )}
-    </Formik>
-  </Modal>
+export const MilestoneModal = AntForm.create({ name: 'milestone_modal' })(
+  MilestoneModalForm
 );
-
-export const MilestoneModal = AntForm.create({ name: 'milestone_modal' })(MilestoneModalForm)
