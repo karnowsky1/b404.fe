0c0b83b2d5d5981cb4f80171bf0acf47dfc37d45
txs4072 <36202906+txs4072@users.noreply.github.com>
2020-03-22 18:31:49 +0100

    qickfix

--- react/src/pages/Signatures.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Signatures extends Component {
-    render() {
-        return (
-          <>Signatures</>
-        )
-    }
-}
--- react/src/routes/Routes.js
+++ react/src/routes/Routes.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Switch, Redirect } from 'react-router-dom'
-import Login from '../components/login/login'
-import Dashboard from '../pages/Dashboard'
-import Documents from '../pages/Documents';
-import Workflow from '../pages/Workflow';
-import Approve from '../pages/Approve';
-import Complete from '../pages/Complete';
-import Submission from '../pages/Submission';
-import Uploads from '../pages/Uploads';
-import Milestones from '../pages/Milestones';
-import Signatures from '../pages/Signatures.js';
-import MainSettings from '../pages/MainSettings';
-import Company from '../pages/Company';
-import People from '../pages/People';
-import AppRoute from './AppRoute'
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Redirect } from "react-router-dom";
+import Login from "../components/login/login";
+import Dashboard from "../pages/Dashboard";
+import Documents from "../pages/Documents";
+import Workflow from "../pages/Workflow";
+import Approve from "../pages/Approve";
+import Complete from "../pages/Complete";
+import Submission from "../pages/Submission";
+import Uploads from "../pages/Uploads";
+import Milestones from "../pages/Milestones";
+import MainSettings from "../pages/MainSettings";
+import Company from "../pages/Company";
+import People from "../pages/People";
+import AppRoute from "./AppRoute";
 
 class Routes extends Component {
   render() {
@@ -28,25 +27,89 @@ class Routes extends Component {
           <PrivateRoute exact path="/signatures" component={Signatures} authed={this.props.isLoggedIn}/>
           <PrivateRoute exact path="/main-settings" component={MainSettings} authed={this.props.isLoggedIn}/>
           <PrivateRoute exact path="/admin" component={Admin} authed={this.props.isLoggedIn}/> */}
-          <AppRoute path={process.env.PUBLIC_URL + "/"} exact component = {Login} />
-          <AppRoute path={process.env.PUBLIC_URL + "/login"} exact component = {Login} />
-          <AppRoute exact path={process.env.PUBLIC_URL + "/dashboard"} component={Dashboard} isPrivate/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/documents"} component={Documents} isPrivate/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/workflow"} component={Workflow} isPrivate/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/approve"} component={Approve} isPrivate/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/complete"} component={Complete} isPrivate/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/submission"} component={Submission} isPrivate/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/upload"} component={Uploads} isPrivate/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/milestones"} component={Milestones} isPrivate/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/signatures"} component={Signatures} isPrivate/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/main-settings"} component={MainSettings} isPrivate/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/company"} component={Company} isPrivate requireAdmin/>
-          <AppRoute exact path={process.env.PUBLIC_URL + "/people"} component={People} isPrivate requireAdmin/>
+          <AppRoute
+            path={process.env.PUBLIC_URL + "/"}
+            exact
+            component={Login}
+          />
+          <AppRoute
+            path={process.env.PUBLIC_URL + "/login"}
+            exact
+            component={Login}
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/dashboard"}
+            component={Dashboard}
+            isPrivate
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/documents"}
+            component={Documents}
+            isPrivate
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/workflow"}
+            component={Workflow}
+            isPrivate
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/approve"}
+            component={Approve}
+            isPrivate
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/complete"}
+            component={Complete}
+            isPrivate
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/submission"}
+            component={Submission}
+            isPrivate
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/upload"}
+            component={Uploads}
+            isPrivate
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/milestones"}
+            component={Milestones}
+            isPrivate
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/main-settings"}
+            component={MainSettings}
+            isPrivate
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/company"}
+            component={Company}
+            isPrivate
+            requireAdmin
+          />
+          <AppRoute
+            exact
+            path={process.env.PUBLIC_URL + "/people"}
+            component={People}
+            isPrivate
+            requireAdmin
+          />
           {/* <AppRoute exact path="/admin" component={Admin} isPrivate requireAdmin/> */}
-          <Redirect to={process.env.PUBLIC_URL + "/dashboard"}/>
+          <Redirect to={process.env.PUBLIC_URL + "/dashboard"} />
         </Switch>
       </BrowserRouter>
-    )
+    );
   }
 }
 
