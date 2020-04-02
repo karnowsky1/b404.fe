aa79896972b85c733dd8f715a3fb12f3c656cda7
Bubba Lichvar <bubbathevtog@gmail.com>
2020-04-01 20:42:48 -0400

    removing unused import

--- react/src/pages/Dashboard.js
+++ react/src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Dash from '../components/dashboard/dashboard';
 
 export default class Dashboard extends Component {
