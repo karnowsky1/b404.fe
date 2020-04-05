3a720a03950383a3b302074d50b70c3e8f9f1772
karnowsky1 <kestime9015@bths.edu>
2020-04-05 17:18:59 -0400

    Modularized the no content section of the milestone component and made it into a separate component. Added it to the dashboard for all 3 sections. Made a messages constant file.  Code styling/ spacing changes.

--- react/src/components/dashboard/dashWork.js
+++ react/src/components/dashboard/dashWork.js
@@ -1,8 +1,13 @@
-import React from "react";
-import axios from "axios";
-import { Button, Progress, Card } from "antd";
-import { TOKEN_KEY /*, UUID_KEY*/ } from "../../constants/auth";
-import { Link } from "react-router-dom";
+import React from 'react';
+import axios from 'axios';
+import { Button, Progress, Card } from 'antd';
+import { TOKEN_KEY /*, UUID_KEY*/ } from '../../constants/auth';
+import { Link } from 'react-router-dom';
+import { NoContent } from '../../utils/NoContent';
+import {
+  noWorkflowsMessageOne,
+  noWorkflowsMessageTwo
+} from '../../constants/messages';
 
 class DashWorkflow extends React.Component {
   constructor(props) {
@@ -24,17 +29,17 @@ class DashWorkflow extends React.Component {
 
   fetch = (params = {}) => {
     axios({
-      method: "get",
-      url: window.__env__.API_URL + "/blink/api/workflow/active",
+      method: 'get',
+      url: window.__env__.API_URL + '/blink/api/workflow/active',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         Authorization: localStorage.getItem(TOKEN_KEY)
       },
       response: {
         results: 4,
         params
       },
-      type: "json"
+      type: 'json'
     })
       .then(response => {
         let conf = [];
@@ -75,26 +80,40 @@ class DashWorkflow extends React.Component {
       <React.Fragment>
         <br />
         <h3>Your Workflows</h3>
-        {this.state.data.map(record => (
-          <div
-            style={{
-              display: "inline-block",
-              padding: "1.2em",
-              textAlign: "center"
-            }}
-            key={record.id}
-          >
-            <Card style={{ width: 265, maxHeight: 300 }}>
-              <Progress type="circle" percent={record.percentComplete * 100} />
-              <p />
-              <p>
-                <b>{record.name}</b>
-              </p>
-              <p>Last Modified:</p>
-              <p>{record.dateM}</p>
-            </Card>
+        {this.state.data[0] ? (
+          this.state.data.map(record => (
+            <div
+              style={{
+                display: 'inline-block',
+                padding: '1.2em',
+                textAlign: 'center'
+              }}
+              key={record.id}
+            >
+              <Card style={{ width: 265, maxHeight: 300 }}>
+                <Progress
+                  type="circle"
+                  percent={record.percentComplete * 100}
+                />
+                <p />
+                <p>
+                  <b>{record.name}</b>
+                </p>
+                <p>Last Modified:</p>
+                <p>{record.dateM}</p>
+              </Card>
+            </div>
+          ))
+        ) : (
+          <div className="dashboard_no_content">
+            <NoContent
+              iconType="file-excel"
+              twoTonecolor="#001529"
+              firstMessage={noWorkflowsMessageOne}
+              secondMessage={noWorkflowsMessageTwo}
+            />
           </div>
-        ))}
+        )}
         <br />
         <div className="viewAllBlockBtn">
           <Button type="default" block>
