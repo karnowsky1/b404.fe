b8384da171e8ad6075b3d4a176bcdbcbc498e15f
maxswa <mkswartwout@gmail.com>
2020-07-21 00:48:44 -0400

    Remove authedRequest in favor of request.

--- frontend/src/api/company.ts
+++ frontend/src/api/company.ts
@@ -1,8 +1,4 @@
-import axios from 'axios';
-import { stringify } from 'querystring';
-
-import { BASE_URL } from '../utils';
-import { authedRequest } from './login';
+import { request } from './request';
 
 export interface Company {
   companyID: number;
@@ -21,110 +17,78 @@ export interface Person {
   signature: string | null;
 }
 
-export const getCompanies = authedRequest((authToken) => async (): Promise<
-  Array<Company>
-> => {
-  const response = await axios.get<Array<Company>>(`${BASE_URL}/company`, {
-    headers: {
-      Authorization: authToken,
-    },
+export const getCompanies = async (): Promise<Array<Company>> => {
+  const response = await request<Array<Company>>({ route: '/company' });
+  return response.data;
+};
+
+export const createCompany = async (name: string): Promise<Company> => {
+  const response = await request<Company>({
+    method: 'POST',
+    route: '/company',
+    body: { name },
   });
   return response.data;
-});
+};
 
-export const createCompany = authedRequest(
-  (authToken) => async (name: string): Promise<Company> => {
-    const response = await axios.post<Company>(
-      `${BASE_URL}/company`,
-      stringify({ name }),
-      {
-        headers: {
-          Authorization: authToken,
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
-    return response.data;
-  }
-);
+export const updateCompany = async (
+  id: number,
+  name: string
+): Promise<Company> => {
+  const response = await request<Company>({
+    method: 'PUT',
+    route: '/company',
+    body: { id, name },
+  });
+  return response.data;
+};
 
-export const updateCompany = authedRequest(
-  (authToken) => async (id: number, name: string): Promise<Company> => {
-    const response = await axios.put<Company>(
-      `${BASE_URL}/company`,
-      stringify({ id, name }),
-      {
-        headers: {
-          Authorization: authToken,
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
-    return response.data;
-  }
-);
+export const getCompany = async (
+  id: number,
+  name: string
+): Promise<Company> => {
+  const response = await request<Company>({
+    route: `/company/id/${id}`,
+  });
+  return response.data;
+};
 
-export const getCompany = authedRequest(
-  (authToken) => async (id: number): Promise<Company> => {
-    const response = await axios.get<Company>(`${BASE_URL}/company/id/${id}`, {
-      headers: {
-        Authorization: authToken,
-      },
-    });
-    return response.data;
-  }
-);
+export const deleteCompany = async (
+  id: number,
+  name: string
+): Promise<Company> => {
+  const response = await request<Company>({
+    method: 'DELETE',
+    route: `/company/id/${id}`,
+  });
+  return response.data;
+};
 
-export const deleteCompany = authedRequest(
-  (authToken) => async (id: number): Promise<void> => {
-    await axios.delete(`${BASE_URL}/company/id/${id}`, {
-      headers: {
-        Authorization: authToken,
-      },
-    });
-  }
-);
+export const getPeopleByCompanyId = async (
+  companyID: number
+): Promise<Array<Person>> => {
+  const response = await request<Array<Person>>({
+    route: `/company/people/${companyID}`,
+  });
+  return response.data;
+};
 
-export const getPeopleByCompanyId = authedRequest(
-  (authToken) => async (companyID: number): Promise<Array<Person>> => {
-    const response = await axios.get<Array<Person>>(
-      `${BASE_URL}/company/people/${companyID}`,
-      {
-        headers: {
-          Authorization: authToken,
-        },
-      }
-    );
-    return response.data;
-  }
-);
+export const addPersonToCompany = async (
+  companyID: number,
+  personID: number
+): Promise<void> => {
+  await request({
+    route: '/company/person/add',
+    body: { companyID, personID },
+  });
+};
 
-export const addPersonToCompany = authedRequest(
-  (authToken) => async (companyID: number, personID: number): Promise<void> => {
-    await axios.post(
-      `${BASE_URL}/company/person/add`,
-      stringify({ companyID, personID }),
-      {
-        headers: {
-          Authorization: authToken,
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
-  }
-);
-
-export const removePersonFromCompany = authedRequest(
-  (authToken) => async (companyID: number, personID: number): Promise<void> => {
-    await axios.post(
-      `${BASE_URL}/company/person/delete`,
-      stringify({ companyID, personID }),
-      {
-        headers: {
-          Authorization: authToken,
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      }
-    );
-  }
-);
+export const removePersonFromCompany = async (
+  companyID: number,
+  personID: number
+): Promise<void> => {
+  await request({
+    route: '/company/person/delete',
+    body: { companyID, personID },
+  });
+};
--- frontend/src/api/login.ts
+++ frontend/src/api/login.ts
@@ -1,7 +1,5 @@
-import axios from 'axios';
-import { stringify } from 'querystring';
-
-import { BASE_URL, AUTH_TOKEN_KEY, hash } from '../utils';
+import { request, setAuthToken } from './request';
+import { hash } from '../utils';
 
 export interface User {
   accessLevelID: number;
@@ -15,40 +13,19 @@ export interface User {
   uuid: string;
 }
 
-let authToken: string | undefined;
-
-export const setAuthToken = (token: string) => {
-  authToken = token;
-  localStorage.setItem(AUTH_TOKEN_KEY, token);
-};
-
 export const login = async (
   username: string,
   password: string
 ): Promise<User> => {
-  const response = await axios.post<User>(
-    `${BASE_URL}/login`,
-    stringify({ username, password: hash(password) }),
-    {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    }
-  );
+  const response = await request<User>({
+    method: 'POST',
+    route: '/login',
+    body: { username, password: hash(password) },
+    unprotected: true,
+  });
   const { Authorization } = response.headers;
   if (Authorization) {
     setAuthToken(Authorization);
   }
   return response.data;
-};
-
-export const authedRequest = <R extends (...args: any) => any>(
-  request: (authToken: string) => R
-) => (...args: Parameters<R>): ReturnType<R> => {
-  if (!authToken) {
-    throw new Error(
-      `No auth token found! User must be authenticated to call ${request.name}.`
-    );
-  }
-  return request(authToken)(...args);
 };
